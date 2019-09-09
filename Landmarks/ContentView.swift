b4ff00dfea21961d34e541b3baf7ed03ee142717
jugomo <julio@iMac.local>
2019-09-09 14:15:36 +0200

    Empty project creation

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by julio on 09/09/2019.
+//  Copyright © 2019 julio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# SwiftUI Tutorials - SwiftUI Essentials
+
+## **Project Name: Landmarks**
+## **Author: Julio González**
+
+### Summary: Sample project using SwiftUI
+
+
