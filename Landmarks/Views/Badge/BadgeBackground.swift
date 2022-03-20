ba78fbf0f99ae4356748979c2164d6a19c090a55
Julio González <>
2022-03-20 12:17:17 +0100

    Redo course using iOS 15 and XCode 13: SwiftUI Draw and anim

---
+++ Landmarks/Views/Badge/BadgeBackground.swift
@@ -0,0 +1,68 @@
+//
+//  BadgeBackground.swift
+//  Landmarks
+//
+//  Created by jugomo on 20/3/22.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+
+        /// get the size of the containig view
+        GeometryReader { geometry in
+            Path { path in
+                /// define container bounds
+                var width: CGFloat = min(geometry.size.width, geometry.size.height)
+                let height = width
+                let xScale: CGFloat = 0.832
+                let xOffset = (width * (1.0 - xScale)) / 2.0
+                width *= xScale
+
+                // add starting point to the path
+                path.move(
+                    to: CGPoint(
+                        x: width * 0.95 + xOffset,
+                        y: height * (0.2 + HexagonParameters.adjustment)
+                    )
+                )
+
+                /// draw the lines for each point of the shape data
+                HexagonParameters.segments.forEach { segment in
+                    path.addLine(
+                        to: CGPoint(
+                            x: width * segment.line.x + xOffset,
+                            y: height * segment.line.y
+                        )
+                    )
+
+                    path.addQuadCurve(
+                        to: CGPoint(
+                            x: width * segment.curve.x + xOffset,
+                            y: height * segment.curve.y
+                        ),
+                        control: CGPoint(
+                            x: width * segment.control.x + xOffset,
+                            y: height * segment.control.y)
+                    )
+                }
+            }
+            .fill(.linearGradient(
+                Gradient(colors: [Self.gradientStart, Self.gradientEnd]),
+                startPoint: UnitPoint(x: 0.5, y: 0),
+                endPoint: UnitPoint(x: 0.5, y: 0.6)
+            ))
+        }
+        .aspectRatio(1, contentMode: .fit)
+    }
+    static let gradientStart = Color(red: 239.0 / 255, green: 120.0 / 255, blue: 221.0 / 255)
+    static let gradientEnd = Color(red: 239.0 / 255, green: 172.0 / 255, blue: 120.0 / 255)
+}
+
+struct BadgeBackground_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeBackground()
+.previewInterfaceOrientation(.portrait)
+    }
+}
